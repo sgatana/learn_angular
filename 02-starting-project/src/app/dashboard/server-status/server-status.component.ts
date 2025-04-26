087f46953d598cdf51c6cf2873fdee97571b348b
sgatana <sgatana@gmail.com>
2025-04-26 17:06:26 +0300

    implement ngOnInit lifecyle

--- 02-starting-project/src/app/dashboard/server-status/server-status.component.ts
+++ 02-starting-project/src/app/dashboard/server-status/server-status.component.ts
@@ -1,12 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import type { ServerStatus } from './server-status.model';
 
 @Component({
   selector: 'app-server-status',
   standalone: true,
   imports: [],
   templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+  styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  constructor() {}
+  currentStatus: ServerStatus = 'online';
+
+  ngOnInit() {
+    setInterval(() => {
+      this.updateStatus();
+    }, 5000);
+  }
+
+  updateStatus() {
+    const statuses = ['online', 'offline', 'unknown'];
+    const randomIndex = Math.floor(Math.random() * statuses.length);
+    this.currentStatus = statuses[randomIndex] as ServerStatus;
+    console.log(`Server status updated to: ${this.currentStatus}`);
+  }
 }
